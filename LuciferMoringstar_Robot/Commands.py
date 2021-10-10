f2cc16c934a41a283a767dcb299f79e97684673c
AlanStalin129 <91825203+AlanStalin129@users.noreply.github.com>
2021-10-10 18:33:47 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        Inline
+                        InlineKeyboardButton('😇OUR GROUP😇', url='https://t.me/vlIeYa9keYUxZDY1'),
                     ]
                     ]
                 await bot.send_cached_media(
