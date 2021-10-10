d1bf260074f01a21af165ea68d2649af62abc7c5
AlanStalin129 <91825203+AlanStalin129@users.noreply.github.com>
2021-10-10 17:43:09 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,8 +30,8 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [Alan Stalin](
+https://t.me/Alan_Sonu_Alan)
 
 ➥ Language : Python3
 
