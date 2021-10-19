3417cc7800eaa233ac1962833afd35a668977722
AlanStalin129 <91825203+AlanStalin129@users.noreply.github.com>
2021-10-19 08:58:14 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -112,7 +112,7 @@ async def start(bot, message):
             )
         )
     else:
-        await message.reply_text(
+        await message.reply_photo(photo=(https://telegra.ph/file/61f07fd0029cb20159118.jpg)
             START_MSG.format(message.from_user.mention),
             parse_mode="Markdown",
             disable_web_page_preview=True,
