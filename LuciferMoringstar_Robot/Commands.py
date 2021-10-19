655ae740739cebf2476ad7a0d754d36793cb889a
AlanStalin129 <91825203+AlanStalin129@users.noreply.github.com>
2021-10-19 10:00:28 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🖥️ Deploy Now 🖥️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🔊MAIN CHANNEL🔊', url='https://t.me/joinchat/axutdh3kmhExZjg1,'),
                     ],
                     [
                         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat='')
@@ -125,7 +125,7 @@ async def start(bot, message):
                             ),
                         InlineKeyboardButton
                             (
-                                "🤖More Botz", url="https://t.me/MT_Botz"
+                                "🎸UPDATE CHANNEL🎸", url="https://t.me/joinchat/ISZ9R5CdkgBlMzNl"
                             )
                     ],
                     [
@@ -346,9 +346,9 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton
+            
                 (
-                     'Video', url=f'{TUTORIAL}'
+                     InlineKeyboardButton('🔊OUR GROUP🔊', url='https://t.me/vlIeYa9keYUxZDY1'),
                 )
         ]
     ]
